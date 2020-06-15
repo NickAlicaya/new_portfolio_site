53f2714855c3eec7c28d3d02ab3da23ccb58cb58
nicanor-alicaya <donalicaya@gmail.com>
2020-06-15 17:26:46 -0400

    added react-mdl,react-router-dom and main component

---
+++ my-app/src/components/Main.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import LandingPage from "./LandingPage"
+// import Route from '
+
+// const Main = () => {
+//     <switch>
+//         <Route  exact path = "/" component ={LandingPage}/>
+//     </switch>
+// }
